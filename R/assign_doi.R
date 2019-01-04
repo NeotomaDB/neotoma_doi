4fedab78aeccc7338b95f32ef0e313d4eac629e0
Simon <simon.j.goring@gmail.com>
2019-01-04 15:00:16 -0800

    Mostly linting for the package to update and prepare for the first tests.

--- R/assign_doi.R
+++ R/assign_doi.R
@@ -1,6 +1,6 @@
 assign_doi <- function(ds_id, post = FALSE) {
 
-  doi_sens <- readr::read_lines('doi_sens.txt')
+  doi_sens <- readr::read_lines("doi_sens.txt")
 
   library(dplyr, quietly = TRUE, verbose = FALSE)
   library(RPostgreSQL, quietly = TRUE, verbose = FALSE)
@@ -15,12 +15,12 @@ assign_doi <- function(ds_id, post = FALSE) {
                   user = doi_sens[4],
               password = doi_sens[5])
 
-  frozen <- fromJSON(paste0('http://api-dev.neotomadb.org/v2.0/data/download/',
+  frozen <- fromJSON(paste0("http://api-dev.neotomadb.org/v2.0/data/download/",
                             ds_id), simplifyVector = FALSE)$data[[1]]
   assertthat::are_equal(frozen$datasetid, ds_id)
 
-  contact <- fromJSON(paste0('http://api-dev.neotomadb.org/v2.0/data/datasets/',
-                             ds_id, '/contacts'),
+  contact <- fromJSON(paste0("http://api-dev.neotomadb.org/v2.0/data/datasets/",
+                             ds_id, "/contacts"),
                       simplifyVector = FALSE)$data[[1]]$contact
 
   schema <- XML::xmlSchemaParse("data//metadata.xsd")
@@ -51,13 +51,14 @@ assign_doi <- function(ds_id, post = FALSE) {
                         .children = list(XML::newXMLNode("creatorName",
                                                     x$fullName),
                                          XML::newXMLNode("affiliation",
-                                                    gsub(pattern = '\r\n', ', ', x$address)))))
+                                                    gsub(pattern = "\r\n", ", ", x$address)))))
            }
           })
 
-  #' Add Titles:
-  title <- paste0(frozen$frozendata$data$dataset$site$sitename, ' ',
-                  frozen$frozendata$data$dataset$dataset$datasettype, ' dataset')
+  # Add Titles:
+  title <- paste0(frozen$frozendata$data$dataset$site$sitename, " ",
+                  frozen$frozendata$data$dataset$dataset$datasettype,
+                  " dataset")
 
   XML::newXMLNode("titles", parent = root)
   XML::newXMLNode("title",
@@ -65,38 +66,41 @@ assign_doi <- function(ds_id, post = FALSE) {
                   attrs = c("xml:lang" = "en-us"),
                   parent = root[["titles"]])
 
-  #' Add publisher information:
-  XML::newXMLNode("publisher", "Neotoma Paleoecological Database", parent = root)
+  # Add publisher information:
+  XML::newXMLNode("publisher", "Neotoma Paleoecological Database",
+    parent = root)
 
-  #' Add publication year:
+  # Add publication year:
   XML::newXMLNode("publicationYear", format(Sys.Date(), "%Y"), parent = root)
 
-  #' Add dataset "Subject" index:
+  # Add dataset "Subject" index:
+  subj_scheme <- c("subjectScheme" = "Library of Congress",
+                       "schemeURI" = "http://id.loc.gov/authorities/subjects")
   XML::newXMLNode("subjects",
                   XML::newXMLNode("subject",
                                   "Paleoecology",
-                                  attrs = c("subjectScheme" = "Library of Congress",
-                                            "schemeURI" = "http://id.loc.gov/authorities/subjects")),
+                                  attrs = subj_scheme),
                   parent = root)
 
-  #' Add in the resource type:
+  # Add in the resource type:
   XML::newXMLNode("resourceType", "Dataset/Paleoecological Sample Data",
                   attrs = c("resourceTypeGeneral" = "Dataset"),
                   parent = root)
 
-  #' Language information.  We assume english at this point.
+  # Language information.  We assume english at this point.
   XML::newXMLNode("language", "English", parent = root)
 
-  #' Get & add contributor information
-  query <- readr::read_file('sql/contact_ids.sql')
+  # Get & add contributor information
+  query <- readr::read_file("sql/contact_ids.sql")
   contacts <- dbGetQuery(con, query, ds_id)
 
   XML::newXMLNode("contributors", parent = root)
 
   lapply(1:nrow(contacts),
          function(x) {
+           ctype <- c("contributorType" = contacts$contributortype[x])
            newXMLNode("contributor",
-                      attrs = c("contributorType" = contacts$contributortype[x]),
+                      attrs = ctype,
                       .children = list(XML::newXMLNode("contributorName",
                                                   contacts$creatorname[x])),
                       parent = root[["contributors"]])
@@ -104,7 +108,7 @@ assign_doi <- function(ds_id, post = FALSE) {
 
   # Adding the dates in one at a time, we use the lapply to insert them
   # into the `dates` node.
-  query <- readr::read_file('sql/mod_dates.sql')
+  query <- readr::read_file("sql/mod_dates.sql")
   dates <- dbGetQuery(con, query, ds_id)
 
   XML::newXMLNode("dates", parent = root)
@@ -116,7 +120,7 @@ assign_doi <- function(ds_id, post = FALSE) {
                       parent = root[["dates"]])
            })
 
-  #' Link to the JSON:
+  # Link to the JSON:
   XML::newXMLNode("relatedIdentifiers", parent = root)
 
   XML::newXMLNode("relatedIdentifier",
@@ -133,7 +137,9 @@ assign_doi <- function(ds_id, post = FALSE) {
                   parent = root[["relatedIdentifiers"]])
 
   # Add DOI tags for the publications as related identifiers:
-  pubs <- fromJSON(paste0("http://api-dev.neotomadb.org/v2.0/data/datasets/", ds_id, "/publications"))
+  pub_loc <- paste0("http://api-dev.neotomadb.org/v2.0/data/datasets/",
+                    ds_id, "/publications")
+  pubs <- fromJSON(pub_loc)
   dois <- na.omit(unlist(pubs$data$doi))
 
   if (length(dois) == 0) {
@@ -148,7 +154,10 @@ assign_doi <- function(ds_id, post = FALSE) {
   }
 
   # Number 13: size
-  size <- as.numeric(object.size(httr::GET(paste0("api-dev.neotomadb.org/v2.0/data/downloads/", ds_id))))
+  dl_loc <- paste0("api-dev.neotomadb.org/v2.0/data/downloads/", ds_id)
+  dl_obj <- httr::GET(dl_loc)
+
+  size <- as.numeric(object.size(dl_obj))
   XML::newXMLNode("sizes",
                   XML::newXMLNode("size", paste0(ceiling(size / 1000), " KB")),
                   parent = root)
@@ -171,9 +180,11 @@ assign_doi <- function(ds_id, post = FALSE) {
                           "xml:lang" = "EN"))
 
   # Number 16
+  rights <- c("rightsURI" =
+    "http://creativecommons.org/licenses/by/4.0/deed.en_US")
   XML::addChildren(XML::newXMLNode("rightsList", parent = root),
                    children = XML::newXMLNode("rights", "CC-BY4",
-                   attrs = c("rightsURI" = "http://creativecommons.org/licenses/by/4.0/deed.en_US")))
+                   attrs = rights))
 
   # Locations
   loc <- fromJSON(frozen$frozendata$data$dataset$site$geography)
@@ -213,25 +224,28 @@ assign_doi <- function(ds_id, post = FALSE) {
   } else {
 
     XML::saveXML(doc = doc,
-                 file = paste0('xml_files/', ds_id, "_output.xml"),
+                 file = paste0("xml_files/", ds_id, "_output.xml"),
                  prefix = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
 
   }
 
   if (post == TRUE) {
 
-    dc_pw <- readr::read_lines('datacite_auth.txt')
+    dc_pw <- readr::read_lines("datacite_auth.txt")
 
-    urlbase <- 'https://mds.test.datacite.org/metadata'
+    urlbase <- "https://mds.test.datacite.org/"
 
     # See documentation at https://support.datacite.org/docs/mds-api-guide
 
+    put_head <- c("Content-Type" = "application/xml;charset=UTF-8",
+                        "Accept" = "text/plain")
+    ul_file <- paste0("xml_files/", ds_id, "_output.xml")
     r = httr::PUT(url = urlbase,
                    # url = paste0(urlbase, "/10.21233/N3"),
-                   config = httr::authenticate(user = dc_pw[1], password = dc_pw[2]),
-                   httr::add_headers(c("Content-Type" = "application/xml;charset=UTF-8",
-                                       "Accept" = "text/plain")),
-                   body = upload_file(paste0('xml_files/', ds_id, '_output.xml'), type ="xml"))
+                   config = httr::authenticate(user = dc_pw[1],
+                                               password = dc_pw[2]),
+                   httr::add_headers(put_head),
+                   body = upload_file(ul_file, type = "xml"))
 
   } else {
 
