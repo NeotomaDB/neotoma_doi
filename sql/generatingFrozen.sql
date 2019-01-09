cb9da37680984b0049c55d8747c9dacf7877ebe7
Simon <simon.j.goring@gmail.com>
2019-01-08 21:35:35 -0800

    Query for checking unfrozen datasets that are over a week old.

---
+++ sql/generatingFrozen.sql
@@ -0,0 +1,14 @@
+INSERT INTO doi.frozen (datasetid, record, recdatecreated)
+WITH ds AS (
+	SELECT DISTINCT ds.datasetid
+	FROM ndb.datasets as ds
+	LEFT OUTER JOIN ndb.datasetdoi as dsdoi ON ds.datasetid = dsdoi.datasetid
+	JOIN ndb.datasetsubmissions AS dss ON dss.datasetid = ds.datasetid
+	WHERE (ds.datasetid, dsdoi.doi) NOT IN (SELECT datasetid, doi FROM doi.frozen) AND
+      	ds.recdatecreated < NOW() - INTERVAL '1 week' AND
+		    dss.submissiondate < NOW() - INTERVAL '1 week'
+)
+SELECT df.datasetid,
+		df.frozendata AS record,
+		current_timestamp AS recdatecreated
+FROM doi.doifreeze((SELECT array_agg(datasetid) FROM ds)) as df
