84e1bfef2cd83440ef6b988d23f5fb84d94d6f82
Simon <simon.j.goring@gmail.com>
2019-01-08 21:37:07 -0800

    Script to check datacite for existing minted DOIs

--- checkdatacite_doi.R
+++ checkdatacite_doi.R
@@ -1,12 +1,12 @@
 #!/usr/bin/Rscript
 
-library(rdatacite)
-library(RPostgreSQL)
-library(stringr)
-library(jsonlite)
-library(dplyr)
+library(rdatacite, quietly = TRUE)
+library(RPostgreSQL, quietly = TRUE)
+library(stringr, quietly = TRUE)
+library(jsonlite, quietly = TRUE)
+suppressMessages(library(dplyr, quietly = TRUE))
 
-con_string <- fromJSON("connect_remote.json")
+con_string <- fromJSON("./connect_remote.json")
 
 con <- dbConnect(PostgreSQL(),
                  host = con_string$host,
@@ -15,11 +15,9 @@ con <- dbConnect(PostgreSQL(),
                  password = con_string$password,
                  dbname = con_string$database)
 
-rows <- 500
+isdoithere <- dbGetQuery(con, "SELECT COUNT(*) FROM ndb.datasetdoi")
 
-isDoiThere <- dbGetQuery(con, "SELECT COUNT(*) FROM ndb.datasetdoi")
-
-if (is.null(isDoiThere)) {
+if (is.null(isdoithere)) {
   create <- "CREATE TABLE ndb.datasetdoi (
     datasetid integer REFERENCES ndb.datasets(datasetid),
     doi character varying,
@@ -27,7 +25,12 @@ if (is.null(isDoiThere)) {
     recdatemodified timestamp
     CONSTRAINT good_doi CHECK (doi ~* '^10.\\d{4,9}/[-._;()/:A-Z0-9]+$')
   )"
-  dbExecute(con, create)
+
+  result <- try(dbExecute(con, create))
+  if (! ("try-error" %in% class(result))) {
+    print("Created doi dataset table.")
+  }
+
 }
 
 existing_dois <- dbGetQuery(con, "SELECT * FROM ndb.datasetdoi")
@@ -35,9 +38,13 @@ existing_dois <- dbGetQuery(con, "SELECT * FROM ndb.datasetdoi")
 check <- FALSE
 doi_set <- list()
 i <- 1
+rows <- 500
+
+# Check datacite for any Neotoma records.  We're paging through the DataCite
+# records, with 500 records per page.
 
 while (check == FALSE) {
-  doi_set[[i]] <- dc_search(q = "publisher:[Neotoma]",
+  doi_set[[i]] <- rdatacite::dc_search(q = "publisher:[Neotoma]",
                             fl = c("doi", "title",
                                    "relatedIdentifier", "uploaded"),
                             rows = rows,
@@ -49,10 +56,13 @@ while (check == FALSE) {
   }
 }
 
-neotoma_dois <- do.call(rbind.data.frame, doi_set) %>% unique()
+neotoma_dois <- do.call(rbind.data.frame, doi_set) %>%
+  unique()
+
+cat(sprintf("Found a total of %d records\n", nrow(neotoma_dois)))
 
 neotoma_dois$dataset <- str_match(neotoma_dois$relatedIdentifier,
-  "downloads/([0-9]*)")[, 2]  %>%
+                                  "downloads/([0-9]*)")[, 2]  %>%
   as.numeric()
 
 neotoma_dois <- neotoma_dois %>%
