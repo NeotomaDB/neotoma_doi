33466903d5f7fc758b88fc24dc4aab8898f74263
Simon <simon.j.goring@gmail.com>
2019-04-08 22:04:17 -0700

    Added logging to scripted_doi when assign_doi fails.
    where we seem to have a data error that drops the function.

--- R/scripted_doi.R
+++ R/scripted_doi.R
@@ -22,7 +22,20 @@ missingdoi <- "SELECT fr.datasetid
 dsids <-  dbGetQuery(con, missingdoi)
 
 for(i in unlist(dsids)) {
-  assign_doi(i, con, post = TRUE, dbpost = FALSE, sandbox = TRUE)
+  output <- try(assign_doi(i, con, post = TRUE, dbpost = FALSE, sandbox = TRUE))
+  if ("try-error" %in% class(output)) {
+    doids <- paste0(Sys.time(), ", ",
+                         i, ", ",
+                          TRUE, ", ",
+                        FALSE, ", ",
+                       TRUE, ",",
+                       as.character(attr(output, "condition")))
+
+    readr::write_lines(doids,
+      path="minting.log",
+      append = TRUE)
+  }
+
   if((which(i == dsids) %% 79) == 0) {
     cat("\n.")
   } else {
