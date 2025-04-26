f9eb728da989500ae2379f059db9068e8d0f810f
Simon <simon.j.goring@gmail.com>
2025-04-25 21:16:41 -0700

    Updating example.

--- neotomadoipy/neotomadoi/example.py
+++ neotomadoipy/neotomadoi/example.py
@@ -5,28 +5,25 @@ import json
 import psycopg2
 import psycopg2.extras
 
-new_doi = neotomadoi.neotomaDOI(datasetid = 16, defaults = 'neotomadoi.yaml')
-new_doi.update()
-#new_doi.validate('../data/datacite_schema.json')
-new_doi.validate()
-
 dotenv.load_dotenv()
 
 DCITE = json.loads(os.getenv('DCITE'))
 
-datacite_meta = {"username": DCITE.get('user'),
-                 "password": DCITE.get('test').get('pw'),
-                 "prefix":  DCITE.get('test').get('handle'),
-                 "test_mode": True}
+datacite_meta = neotomadoi.credentials(DCITE)
 
+new_doi = neotomadoi.neotomaDOI(datasetid = 16, defaults = 'neotomadoi.yaml')
 new_doi.set_user(datacite_meta)
-# new_doi.identifiers = None
-# new_doi.mint_doi()
+
+new_doi.update()
+new_doi.validate()
 
 con = neotomadoi.neo_connect()
 
-query = """SELECT doi.* FROM doi.doimeta AS doi
-           inner join ndb.datasets AS ds ON ds.datasetid = doi.datasetid;"""
+query = """SELECT ds.datasetid
+           FROM ndb.datasets AS ds
+           LEFT JOIN doi.doimeta AS dom ON dom.datasetid = ds.datasetid
+           WHERE dom.datasetid IS NULL
+           AND NOT ds.datatypeid = 1;"""
 
 with con.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
     cur.execute(query)
